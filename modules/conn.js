758c497f9a772abaa2b09d11166f1a4ce63f011d
Abinash Ahir <ahirabinash@gmail.com>
2021-11-21 17:05:58 +0530

    edit1

--- modules/conn.js
+++ modules/conn.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 
 // - connect localhost...
 // const conn = mongoose.connect(`mongodb://localhost:27017/Basic_Banking_System`);
-// - connect localhost...
-// const conn = mongoose.connect(`mongodb+srv://admin-abinash:Ahir123@cluster0.is5lo.mongodb.net/Basic_Banking_System`);
+
 
 // - connect mongo atlas
 const conn = mongoose.connect(process.env.DB);
