8be7f8788885d09587ab2940b50de717f5fb4d40
Abinash Ahir <ahirabinash@gmail.com>
2021-11-21 16:19:38 +0530

    connected with mongoDB

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const hostname = 'localhost'
 const port = process.env.PORT || 5000;
 
 
-app.set('view-engine','ejs');
+app.set('view-engine', 'ejs');
 app.use(express.static("public"));
 
 // parse application/x-www-form-urlencoded
@@ -17,10 +17,6 @@ app.use(bodyParser.json());
 require("./modules/conn");
 app.use(require("./routes/index"));
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`server run at http://${hostname}:${port}/`)
 });
-
-
-
-
--- modules/conn.js
+++ modules/conn.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 
 // - connect localhost...
 // const conn = mongoose.connect(`mongodb://localhost:27017/Basic_Banking_System`);
+// - connect localhost...
+const conn = mongoose.connect(`mongodb+srv://admin-abinash:Ahir123@cluster0.is5lo.mongodb.net/Basic_Banking_System`);
 
 // - connect mongo atlas
-const conn = mongoose.connect(process.env.DB);
+// const conn = mongoose.connect(process.env.DB);
 
 module.exports = conn;
-
